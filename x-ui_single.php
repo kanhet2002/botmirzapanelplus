bf33ee6d29304687567192d6fa47750a5bea9c81
kanhet2002 <kanhet2002@gmail.com>
2024-10-17 02:10:10 -0700

    Update x-ui_single.php

--- x-ui_single.php
+++ x-ui_single.php
@@ -1,261 +1,205 @@
 <?php
 require_once 'config.php';
 
-
-function login($url,$username,$password){
-$curl = curl_init();
-curl_setopt_array($curl, array(
-  CURLOPT_URL => $url.'/login',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT_MS => 4000,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS => "username=$username&password=$password",
-  CURLOPT_COOKIEJAR => 'cookie.txt',
-));
-$response = curl_exec($curl);
-if (curl_error($curl)) {
+function login($url, $username, $password) {
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $url . '/login',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT_MS => 4000,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => "username=$username&password=$password",
+        CURLOPT_COOKIEJAR => 'cookie.txt',
+    ));
+    $response = curl_exec($curl);
+    if (curl_error($curl)) {
         $token = [];
-        $token['errror'] = curl_error($curl);
+        $token['error'] = curl_error($curl);
         return $token;
     }
-curl_close($curl);
-return json_decode($response,true);
+    curl_close($curl);
+    return json_decode($response, true);
 }
 
-
-function get_Client($username,$namepanel){
+function get_Client($username, $namepanel) {
     global $connect;
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel,"select");
-    login($marzban_list_get['url_panel'],$marzban_list_get['username_panel'],$marzban_list_get['password_panel']);
+    $panel_info = select("marzban_panel", "*", "name_panel", $namepanel, "select");
+    login($panel_info['url_panel'], $panel_info['username_panel'], $panel_info['password_panel']);
     $curl = curl_init();
-
-curl_setopt_array($curl, array(
-  CURLOPT_URL => $marzban_list_get['url_panel'].'/panel/api/inbounds/getClientTraffics/'.$username,
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
-  CURLOPT_HTTPHEADER => array(
-    'Accept: application/json'
-  ),
-  CURLOPT_COOKIEFILE => 'cookie.txt',
-));
-$output = [];
-$response = json_decode(curl_exec($curl),true)['obj'];
-curl_close($curl);
-return $response;
-unlink('cookie.txt');
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $panel_info['url_panel'] . '/panel/api/inbounds/getClientTraffics/' . $username,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER => array('Accept: application/json'),
+        CURLOPT_COOKIEFILE => 'cookie.txt',
+    ));
+    $response = json_decode(curl_exec($curl), true)['obj'];
+    curl_close($curl);
+    unlink('cookie.txt');
+    return $response;
 }
-function get_clinets($username,$namepanel){
-    global $connect;
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel,"select");
-    login($marzban_list_get['url_panel'],$marzban_list_get['username_panel'],$marzban_list_get['password_panel']);
-    $curl = curl_init();
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => $marzban_list_get['url_panel'].'/panel/api/inbounds/list',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
-  CURLOPT_HTTPHEADER => array(
-    'Accept: application/json'
-  ),
-  CURLOPT_COOKIEFILE => 'cookie.txt',
-));
-$output = [];
-$response = json_decode(curl_exec($curl),true)['obj'];
-foreach ($response as $client){
-    $client= json_decode($client['settings'],true)['clients'];
-    foreach($client as $clinets){
-    if($clinets['email'] == $username){
-        $output = $clinets;
-        break;
+function get_clinets($username, $namepanel) {
+    global $connect;
+    $panel_info = select("marzban_panel", "*", "name_panel", $namepanel, "select");
+    login($panel_info['url_panel'], $panel_info['username_panel'], $panel_info['password_panel']);
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $panel_info['url_panel'] . '/panel/api/inbounds/list',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER => array('Accept: application/json'),
+        CURLOPT_COOKIEFILE => 'cookie.txt',
+    ));
+    $response = json_decode(curl_exec($curl), true)['obj'];
+    foreach ($response as $client) {
+        $client = json_decode($client['settings'], true)['clients'];
+        foreach ($client as $clinets) {
+            if ($clinets['email'] == $username) {
+                $output = $clinets;
+                break;
+            }
+        }
     }
-    }
+    curl_close($curl);
+    unlink('cookie.txt');
+    return $output;
+}
+
+function addClient($namepanel, $usernameac, $Expire, $Total, $Uuid, $Flow, $subid) {
+    global $connect;
+    $panel_info = select("marzban_panel", "*", "name_panel", $namepanel, "select");
+    $Allowedusername = get_Client($usernameac, $namepanel);
     
-}
-curl_close($curl);
-unlink('cookie.txt');
-return $output;
-}
-function addClient($namepanel, $usernameac, $Expire,$Total, $Uuid,$Flow,$subid){
-    global $connect;
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel,"select");
-    $Allowedusername = get_Client($usernameac,$namepanel);
+    // ایجاد نام کاربری جدید در صورت تکراری
     if (isset($Allowedusername['email'])) {
         $random_number = rand(1000000, 9999999);
         $username_ac = $usernameac . $random_number;
     }
-    login($marzban_list_get['url_panel'], $marzban_list_get['username_panel'], $marzban_list_get['password_panel']);
+
+    login($panel_info['url_panel'], $panel_info['username_panel'], $panel_info['password_panel']);
+    
+    // پیکربندی برای اضافه کردن کاربر
     $config = array(
-        "id" => intval($marzban_list_get['inboundid']),
+        "id" => intval($panel_info['inboundid']),
         'settings' => json_encode(array(
             'clients' => array(
                 array(
-                "id" => $Uuid,
-                "flow" => $Flow,
-                "email" => $usernameac,
-                "totalGB" => $Total,
-                "expiryTime" => $Expire,
-                "enable" => true,
-                "tgId" => "",
-                "subId" => $subid,
-                "reset" => 0
-            )),
-             'decryption' => 'none',
+                    "id" => $Uuid,
+                    "flow" => $Flow,
+                    "email" => $username_ac,
+                    "totalGB" => $Total,
+                    "expiryTime" => $Expire,
+                    "enable" => true,
+                    "tgId" => "",
+                    "subId" => $subid,
+                    "reset" => 0
+                )
+            ),
+            'decryption' => 'none',
             'fallbacks' => array(),
         ))
-        );
+    );
 
-    $configpanel = json_encode($config,true);
-
+    $configpanel = json_encode($config, true);
     $curl = curl_init();
     curl_setopt_array($curl, array(
-        CURLOPT_URL => $marzban_list_get['url_panel'].'/panel/api/inbounds/addClient',
+        CURLOPT_URL => $panel_info['url_panel'] . '/panel/api/inbounds/addClient',
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => 'POST',
         CURLOPT_POSTFIELDS => $configpanel,
         CURLOPT_COOKIEFILE => 'cookie.txt',
-        CURLOPT_SSL_VERIFYPEER => false,
         CURLOPT_HTTPHEADER => array(
             'Accept: application/json',
             'Content-Type: application/json',
         ),
     ));
     $response = curl_exec($curl);
-
     curl_close($curl);
     unlink('cookie.txt');
     return json_decode($response, true);
 }
-function updateClient($namepanel, $username,array $config){
-    global $connect;
-    $UsernameData = get_clinets($username,$namepanel);
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel,"select");
-    login($marzban_list_get['url_panel'], $marzban_list_get['username_panel'], $marzban_list_get['password_panel']);
-    $configpanel = json_encode($config,true);
 
+// تابع برای پنل علیرضا
+function loginAlireza($url, $username, $password) {
     $curl = curl_init();
     curl_setopt_array($curl, array(
-        CURLOPT_URL => $marzban_list_get['url_panel'].'/panel/api/inbounds/updateClient/'.$UsernameData['id'],
+        CURLOPT_URL => $url . '/login',
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
+        CURLOPT_TIMEOUT_MS => 4000,
         CURLOPT_FOLLOWLOCATION => true,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => "username=$username&password=$password",
+        CURLOPT_COOKIEJAR => 'cookie.txt',
+    ));
+    $response = curl_exec($curl);
+    if (curl_error($curl)) {
+        $token = [];
+        $token['error'] = curl_error($curl);
+        return $token;
+    }
+    curl_close($curl);
+    return json_decode($response, true);
+}
+
+// سایر توابع مشابه برای پنل علیرضا
+function get_Client_Alireza($username) {
+    global $connect;
+    $panel_info = select("alireza_panel", "*", "name_panel", "Alireza", "select");
+    loginAlireza($panel_info['url_panel'], $panel_info['username_panel'], $panel_info['password_panel']);
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $panel_info['url_panel'] . '/api/inbounds/getClientTraffics/' . $username,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER => array('Accept: application/json'),
+        CURLOPT_COOKIEFILE => 'cookie.txt',
+    ));
+    $response = json_decode(curl_exec($curl), true)['obj'];
+    curl_close($curl);
+    unlink('cookie.txt');
+    return $response;
+}
+
+function addClient_Alireza($usernameac, $Expire, $Total, $Uuid, $Flow, $subid) {
+    global $connect;
+    $panel_info = select("alireza_panel", "*", "name_panel", "Alireza", "select");
+    
+    loginAlireza($panel_info['url_panel'], $panel_info['username_panel'], $panel_info['password_panel']);
+    
+    // پیکربندی برای اضافه کردن کاربر
+    $config = array(
+        'email' => $usernameac,
+        'totalGB' => $Total,
+        'expiryTime' => $Expire,
+        'id' => $Uuid,
+        'flow' => $Flow,
+        'subId' => $subid,
+        'enable' => true
+    );
+
+    $configpanel = json_encode($config, true);
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $panel_info['url_panel'] . '/api/inbounds/addClient',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_CUSTOMREQUEST => 'POST',
         CURLOPT_POSTFIELDS => $configpanel,
         CURLOPT_COOKIEFILE => 'cookie.txt',
-        CURLOPT_SSL_VERIFYPEER => false,
         CURLOPT_HTTPHEADER => array(
             'Accept: application/json',
             'Content-Type: application/json',
         ),
     ));
-
     $response = curl_exec($curl);
-
     curl_close($curl);
     unlink('cookie.txt');
     return json_decode($response, true);
 }
-function ResetUserDataUsagex_uisin($usernamepanel, $namepanel){
-    global $connect;
-    $data_user = get_clinets($usernamepanel,$namepanel);
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel,"select");
-    login($marzban_list_get['url_panel'], $marzban_list_get['username_panel'], $marzban_list_get['password_panel']);
-    $curl = curl_init();
-curl_setopt_array($curl, array(
-  CURLOPT_URL => $marzban_list_get['url_panel']."/panel/api/inbounds/{$marzban_list_get['inboundid']}/resetClientTraffic/".$data_user['email'],
-  CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS => $configpanel,
-        CURLOPT_COOKIEFILE => 'cookie.txt',
-        CURLOPT_SSL_VERIFYPEER => false,
-        CURLOPT_HTTPHEADER => array(
-            'Accept: application/json',
-            'Content-Type: application/json',
-        ),
 
-));
-
-$response = curl_exec($curl);
-curl_close($curl);
-unlink('cookie.txt');
-}
-function removeClient($location,$username){
-    global $connect;
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $location,"select");
-    login($marzban_list_get['url_panel'], $marzban_list_get['username_panel'], $marzban_list_get['password_panel']);
-    $data_user = get_clinets($username,$location);
-    login($marzban_list_get['url_panel'], $marzban_list_get['username_panel'], $marzban_list_get['password_panel']);
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-  CURLOPT_URL => $marzban_list_get['url_panel']."/panel/api/inbounds/{$marzban_list_get['inboundid']}/delClient/".$data_user['id'],
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_COOKIEFILE => 'cookie.txt',
-  CURLOPT_HTTPHEADER => array(
-    'Accept: application/json',
-  ),
-));
-
-$response = json_decode(curl_exec($curl),true);
-curl_close($curl);
-unlink('cookie.txt');
-return $response;
-}
-function get_onlinecli($name_panel,$username){
-    global $connect;
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $name_panel,"select");
-    login($marzban_list_get['url_panel'],$marzban_list_get['username_panel'],$marzban_list_get['password_panel']);
-    $curl = curl_init();
-curl_setopt_array($curl, array(
-  CURLOPT_URL => $marzban_list_get['url_panel'].'/panel/api/inbounds/onlines',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_HTTPHEADER => array(
-    'Accept: application/json'
-  ),
-  CURLOPT_COOKIEFILE => 'cookie.txt',
-));
-$response = json_decode(curl_exec($curl),true)['obj'];
-if($response == null)return "offline";
-if(in_array($username,$response))return "online";
-return "offline";
-curl_close($curl);
-unlink('cookie.txt');
-
-}
+// توابع دیگر برای مدیریت کاربران در پنل علیرضا می‌توانید بر اساس الگوی موجود اضافه کنید
+?>
